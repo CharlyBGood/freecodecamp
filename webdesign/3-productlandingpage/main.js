96225b269b79487ffc568f0d0e6008116b4f8044
charlybgood <bonavita.carlos@gmail.com>
2022-09-20 17:23:37 -0300

    add javascript and modify css responsive

--- webdesign/3-productlandingpage/main.js
+++ webdesign/3-productlandingpage/main.js
@@ -1,8 +1,31 @@
-function myHumb() {
-    let x = document.getElementById("nav-bar");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myHumb() {
+//   let x = document.getElementById("nav-bar");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+let toggleBtn = document.getElementsByClassName("navbar_toggle")[0];
+let navbar = document.getElementById("nav-bar");
+let links = document.querySelectorAll(".nav-link");
+
+links.forEach(link => link.addEventListener("click", ()=> {
+    navbar.classList.remove("responsive")
+    toggleBtn.style.display = "flex"
+    console.log("clicked link")
+}))
+
+
+toggleBtn.addEventListener("click", () => {
+    console.log("yes")
+    x.className += " responsive"
+    toggleBtn.style.display = "none"    
+})   
+
+
+let call = document.getElementById("call");
+call.addEventListener("click", () => {
+  alert("Gracias por fingir que nos estas llamando!");
+});
